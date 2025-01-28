f2eeb91c6fbf79b88bc451367c3fb2551ae927ea
GersonAparecidoLima <gersonaparecido@gmail.com>
2025-01-28 10:56:43 -0300

    programa tela interacao

--- PrimeitoProjeto/Program.cs
+++ PrimeitoProjeto/Program.cs
@@ -4,8 +4,10 @@ string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 //Criando uma lista
 //List<String> ListaDasBandas = new List<string>();
 //Carregando a lista
-List<String> ListaDasBandas = new List<string> { "Harpa Cristã"};
-
+//List<String> ListaDasBandas = new List<string> { "Harpa Cristã"};
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+bandasRegistradas.Add("Fernadinho",new List<int> { 10,8,9});
+bandasRegistradas.Add("Marquinhos Gomes", new List<int>());
 
 //https://fsymbols.com/pt/geradores/
 //quando utilizamos o @ antes
@@ -32,10 +34,10 @@ void ExibirLogo() {
 
 void ExibirOpcoesDoMenu() {
     ExibirLogo();
-    Console.WriteLine("\n Digite 1 para registra uma banda" );
-    Console.WriteLine("Digite 2 para mostra todas as bandas");
-    Console.WriteLine("Digite 3 para para avaliar uma banda");
-    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("\nDigite 1 para registra uma banda: " );
+    Console.WriteLine("Digite 2 para mostra todas as bandas: ");
+    Console.WriteLine("Digite 3 para para avaliar uma banda: ");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda: ");
     Console.WriteLine("Digite -1 para sair");
 
     Console.WriteLine( "\n Digite a sua opção :");
@@ -58,13 +60,13 @@ void ExibirOpcoesDoMenu() {
             MostrarBandasRegistras();
             break;
         case 3:
-            Console.WriteLine("Voce digitou a opção " + opcaoEscolhida);
+            AvaliarUmaBanda();
             break;
         case 4:
-            Console.WriteLine("Voce digitou a opção " + opcaoEscolhida);
+            ExibirMedia();
             break;
         case - 1:
-            Console.WriteLine("Voce digitou a opção " + opcaoEscolhida);
+            Console.WriteLine("Tchau tchau");
             break;
         default:
             Console.WriteLine("opcao invalida " + opcaoEscolhida);
@@ -78,12 +80,19 @@ void RegistrarBanda()
 {
     //limpando
     Console.Clear();
-    Console.WriteLine("*************************");
-    Console.WriteLine("Registro de Banda ");
-    Console.WriteLine("*************************");
+    ExibirTituloDaOpcao("Registro de Banda");
     Console.Write("Digite o nome da banda que deseja registra ");
     String nomeDaBanda = Console.ReadLine()!;
-    ListaDasBandas.Add(nomeDaBanda);
+
+    //estamos adicionando elemento para Lista
+    //ListaDasBandas.Add(nomeDaBanda);
+
+    //estamos adicionando elemento para dicionario
+    //lembando que temos dois argumentos, no 
+    //caso o nome(chave), e uma lista de notas
+    bandasRegistradas.Add(nomeDaBanda, new List<int>());
+
+
     //itilizando interpolação de string
     //começa com $ e onde esta a variaveil coloco a chaves{}
     Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso");
@@ -98,16 +107,14 @@ void RegistrarBanda()
 void MostrarBandasRegistras()
 {
     Console.Clear();
-    Console.WriteLine("*************************");
-    Console.WriteLine("Exibindo todas as bandas registradas");
-    Console.WriteLine("*************************\n");
+    ExibirTituloDaOpcao("Exibindo todas as bandas registradas");
     //percore um lista
     //for (int i = 0; i < listaDasBandas.Count; i++)
     //{
     //Console.WriteLine($"Banda: {listaDasBandas[i]}");
     //}
 
-    foreach (string banda in ListaDasBandas)
+    foreach (string banda in bandasRegistradas.Keys)
     {
         Console.WriteLine($"Banda: {banda}");
     }
@@ -115,6 +122,85 @@ void MostrarBandasRegistras()
     Console.ReadKey();
     Console.Clear();
     ExibirOpcoesDoMenu();
+
+
+
+}
+
+void ExibirTituloDaOpcao( string titulo)
+{
+    //buscando a quantidade de letra do titulo
+    int quantidadeDeLetra = titulo.Length;
+
+    //criando uma string vazia
+    //PadLeft adicionando quantidade de caracter a esquerda
+    //passa dois parametros 1 quantidade de caracter 2 e o caracter
+    string asterisco = string.Empty.PadLeft(quantidadeDeLetra, '*');
+    Console.WriteLine(asterisco);
+    Console.WriteLine(titulo);
+    Console.WriteLine(asterisco + "\n");
+
+}
+
+void AvaliarUmaBanda() { 
+//digite qual banda deseja avaliar
+//sea a banda exitir no dicionario >> atribuir uma nota
+//senão, volta ao menu principal
+
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    //quando colocamos o ! nos queremos uma string e não um valor
+    string nomeDaBanda = Console.ReadLine()!;
+    //verificado se tem a banda na lista
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        //Quando eu utilizo colchete eu acesso a segunda parte do dicionario
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine( $"\nA nota {nota} foi registrada com sucesso para a banda{nomeDaBanda}" );
+        //esperando
+        Thread.Sleep(4000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encotrado!");
+        Console.WriteLine("Digite uam tecla para volta ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+}
+
+void ExibirMedia()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Exibir média da banda");
+    Console.WriteLine("Digite o nome da banda que deseja exibir a média");
+    string nomeDaBanda = Console.ReadLine()!;
+    //Verifica se a banda existe
+    if (bandasRegistradas.ContainsKey(nomeDaBanda)) {
+    
+    List<int> notasDaBanda = bandasRegistradas [nomeDaBanda];
+        Console.WriteLine($"\nA média da banda {nomeDaBanda} é {notasDaBanda.Average()} ");
+        Console.WriteLine("Digite uma tecla para volta para ao menu principla");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+
+    }
+    else
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encotrado!");
+        Console.WriteLine("Digite uam tecla para volta ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+
+    }
 
 
 
